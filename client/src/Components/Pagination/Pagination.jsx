66a11575229b652ce5d840e1a5694fb1dbb9a066
mantezana <maximo00antezana@gmail.com>
2022-04-13 13:57:34 -0700

    pagination props passed

--- client/src/Components/Pagination/Pagination.jsx
+++ client/src/Components/Pagination/Pagination.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import { Pagination } from 'semantic-ui-react'
 
-const PaginationExamplePagination = () => (
-  <Pagination defaultActivePage={5} totalPages={20} />
-)
+class Paginate {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      totalPages: null,
+      dataStartingIndex: null,
+      dataLastIndex: null,
+      currentClickedNumber: 1,
+      pageData: null,
+    }
+  }
+
+  paginationExamplePagination = () => (
+    <Pagination 
+      defaultActivePage={5} 
+      totalPages={20} 
+      />
+  )
+
+}
+
 
 export default PaginationExamplePagination
