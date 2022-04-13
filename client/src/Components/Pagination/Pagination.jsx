38d38ae3990c01246341ea6a4e7eb6a39f3bc447
mantezana <maximo00antezana@gmail.com>
2022-04-12 18:41:16 -0700

    pagination added

--- client/src/Components/Pagination/Pagination.jsx
+++ client/src/Components/Pagination/Pagination.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Pagination } from 'semantic-ui-react'
 
 const PaginationExamplePagination = () => (
-  <Pagination defaultActivePage={5} totalPages={10} />
+  <Pagination defaultActivePage={5} totalPages={20} />
 )
 
 export default PaginationExamplePagination
