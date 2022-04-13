827e2090a002e53b5527b2b1c999ca907eb4b293
mantezana <maximo00antezana@gmail.com>
2022-04-12 17:55:02 -0700

    fetch api complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  const foamPage = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/4fc14642-a3d6-424b-b621-5ecf5d955d7f/foam-seed.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220413%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220413T003306Z&X-Amz-Expires=86400&X-Amz-Signature=6d69bccc9dd7f39a98dbceda38d0dd7a887abdbb16abdd5a55be3a5cc5f7111d&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22foam-seed.json%22&x-id=GetObject";
+
+  const fetchData = () => {
+    fetch(foamPage)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      setData(data)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Foam Take Home Challenge</h1>
   );
 }
 
