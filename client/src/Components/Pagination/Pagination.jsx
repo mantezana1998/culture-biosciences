5fd50912bdeaea4e772af6a313a4380a803528e5
mantezana <maximo00antezana@gmail.com>
2022-04-13 17:36:06 -0700

    page tabs not working

--- client/src/Components/Pagination/Pagination.jsx
+++ client/src/Components/Pagination/Pagination.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import { Pagination } from 'semantic-ui-react'
 
-  function PaginationExamplePagination({data}){
+  function PaginationExamplePagination(){
+
+    function handlePageChange(data){
+      console.log(data.selected)
+    }
+
     return(
-      <Pagination />
+      <Pagination 
+        boundaryRange={0}
+        defaultActivePage={1}
+        ellipsisItem={null}
+        firstItem={null}
+        lastItem={null}
+        siblingRange={1}
+        totalPages={20}
+        onClick={handlePageChange}
+      />
     )
   }
 
